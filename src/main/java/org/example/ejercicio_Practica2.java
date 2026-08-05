e08f2cb6235315e5cb3d11085404493f33e70ada
Salazar@7QWNVV3 <brenda.jsp18@gmail.com>
2026-08-05 11:36:12 -0600

    Se cambio la descripción del println

--- src/main/java/org/example/ejercicio_Practica2.java
+++ src/main/java/org/example/ejercicio_Practica2.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class ejercicio_Practica2 {
     public static void main(String[] args){
-        System.out.println("Este es un ejercicio de práctica");
+        System.out.println("Este es un ejercicio de práctica :) ");
     }
 }
